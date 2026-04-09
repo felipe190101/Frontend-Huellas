b50128a60787e98dbd1b8ca8eb39310d043e70e9
Felipe Moreno <pipeguerrero671@gmail.com>
2026-04-09 16:23:29 -0500

    correción scroll

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,17 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter, withInMemoryScrolling } from '@angular/router'; // 1. Importa esto
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(
+      routes,
+      // 2. Agrega esta configuración aquí:
+      withInMemoryScrolling({ 
+        scrollPositionRestoration: 'top' 
+      })
+    )
   ]
 };
